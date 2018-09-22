4f2ea4a5e486e5fd9982b8a0001f5a28571a86fa
yongqiang(丁勇强) <yongqiang@futunn.com>
2018-09-22 21:11:15 +0800

    no message

--- pages/activityPublish/activityPublish.js
+++ pages/activityPublish/activityPublish.js
@@ -5,11 +5,31 @@ const LOG_TAG = 'ActivityPublish ';
 Page({
     data: {
         publishBtnEnabled: true,
+        placeholder: '活动标题',
+        focus: false,
+        type: 'text',
+        disabled: false,
+        maxlength: 30,
+        value: '',
+        bindchange: 'bindTitleInputChanged',
+        bindinput: 'bindTitleInputFocus',
+        bindfocus: 'bindTitleInputInput',
+        bindblur: 'bindTitleInputBlur',
     },
 
     onLoad: function () {
 
     },
+
+    bindTitleInputChanged: function(e) {},
+
+    bindTitleInputFocus: function(e) {},
+
+    bindTitleInputInput: function(e) {
+        let searchText = e.detail.value;
+    },
+
+    bindTitleInputBlur: function(e) {},
 
     bindPublishButton: function (e) {
         let info = {
